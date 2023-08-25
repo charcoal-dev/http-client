d9f517ab8593230bb84d62c1a0bcf7c22e8dca9e
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-08-26 01:03:41 +0400

    exceptions

---
+++ src/Exception/SecureRequestException.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/http-client" package.
+ * https://github.com/charcoal-dev/http-client
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/http-client/blob/main/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\HTTP\Client\Exception;
+
+/**
+ * Class SecureRequestException
+ * @package Charcoal\HTTP\Client\Exception
+ */
+class SecureRequestException extends RequestException
+{
+}
