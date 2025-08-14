13c5f82b3a67e0561d100f67357d0d7c7e83dc37
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-14 19:34:17 +0500

    Simplify exception class headers and update `HttpClientException` to extend `\Exception`.

--- src/Exception/HttpClientException.php
+++ src/Exception/HttpClientException.php
@@ -1,25 +1,17 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/http-client" package.
- * https://github.com/charcoal-dev/http-client
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/http-client/blob/main/LICENSE
+/**
+ * Part of the "charcoal-dev/http-client" package.
+ * @link https://github.com/charcoal-dev/http-client
  */
 
 declare(strict_types=1);
 
 namespace Charcoal\Http\Client\Exception;
 
-use Charcoal\Http\Commons\Exception\HttpException;
-
 /**
  * Class HttpClientException
  * @package Charcoal\Http\Client\Exception
  */
-class HttpClientException extends HttpException
+class HttpClientException extends \Exception
 {
 }
--- src/Exception/RequestException.php
+++ src/Exception/RequestException.php
@@ -1,13 +1,7 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/http-client" package.
- * https://github.com/charcoal-dev/http-client
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/http-client/blob/main/LICENSE
+/**
+ * Part of the "charcoal-dev/http-client" package.
+ * @link https://github.com/charcoal-dev/http-client
  */
 
 declare(strict_types=1);
